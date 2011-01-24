f9c42f620d730755638493d5dc163e77e15b0b2e
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-01-23 22:33:28 -0500

    Updating the prep files

--- build/local_properties.rb
+++ build/local_properties.rb
@@ -1,6 +1,16 @@
 class LocalSettings 
-  attr_reader :settings
-  def initialize
-    @settings = {}
-  end
+ def create_settings_dictionary()
+  settings = {
+  	:app_config_template => "app.config.template" ,
+  	:path_to_runtime_log4net_config => File.join(%w[artifacts log4net.config.xml]),
+  	:asp_net_worker_process => "aspnet_wp.exe",
+  	:log_file_name => "prep_log.txt",
+  	:log_level => "DEBUG",
+  	:xunit_report_file_dir => "artifacts" ,
+  	:xunit_report_file_name => "test_report",
+  	:xunit_report_type => "text",
+  	:xunit_show_test_report => true,
+  	:debug => "TRUE",
+  }
+ end
 end
