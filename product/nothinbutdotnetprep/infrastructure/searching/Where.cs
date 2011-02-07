a64593fcaf720bf5311a26387f893d192379046b
Steve Brettschneider <steve.brettschneider@pinnsg.com>
2011-02-07 15:21:48 -0500

    Pushing new changes

---
+++ product/nothinbutdotnetprep/infrastructure/searching/Where.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nothinbutdotnetprep.infrastructure.searching
+{
+    public class Where<T>
+    {
+        public Criteria<T> has_a(Condition<T> condition)
+        {
+            return new AnonymousCriteria<T>(condition);
+        }
+    }
+
+    public delegate object GetProperty()
+
+    public static class CriteriaExtentions
+    {
+        public static Criteria<T> equal_to<T>(object o)
+        {
+            return null;
+        }
+    }
+}
