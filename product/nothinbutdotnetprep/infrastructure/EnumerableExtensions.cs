e607c408b8fb92cb962402531c86985a551e2380
Andrei <avieru@gmail.com>
2011-02-08 10:44:29 -0500

    Pushing new changes

--- product/nothinbutdotnetprep/infrastructure/EnumerableExtensions.cs
+++ product/nothinbutdotnetprep/infrastructure/EnumerableExtensions.cs
@@ -23,5 +23,27 @@ namespace nothinbutdotnetprep.infrastructure
         {
             return all_items_matching(items, criteria.matches);
         }
+
+        public static EnumerableCriteriaFactory<ItemToFilter,PropertyType> where<ItemToFilter,PropertyType>(this IEnumerable<ItemToFilter> enumerable,PropertyAccessor<ItemToFilter, PropertyType> accessor)
+        {
+            return new EnumerableCriteriaFactory<ItemToFilter, PropertyType>(enumerable, accessor);
+        }
+    }
+
+    public class EnumerableCriteriaFactory<ItemToFilter, PropertyType> 
+    {
+        readonly IEnumerable<ItemToFilter> enumerable;
+        readonly PropertyAccessor<ItemToFilter, PropertyType> accessor;
+
+        public EnumerableCriteriaFactory(IEnumerable<ItemToFilter> enumerable, PropertyAccessor<ItemToFilter, PropertyType> accessor)
+        {
+            this.enumerable = enumerable;
+            this.accessor = accessor;
+        }
+
+        public IEnumerable<ItemToFilter> equal_to(PropertyType value_to_check_against)
+        {
+            return enumerable.all_items_matching(Where<ItemToFilter>.has_a(accessor).equal_to(value_to_check_against));
+        }
     }
 }
